3463f6ead4c1cafac2759a43f5c299e3bd2a4bfd
and3ss <anderson.aluizio@outlook.com>
2024-12-27 10:58:00 -0300

    Add portal relationship to Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $guarded = [];
+
+    public function portal()
+    {
+        return $this->belongsTo(Portal::class);
+    }
 }
