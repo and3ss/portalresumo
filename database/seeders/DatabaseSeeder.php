51026ec695e7d1ad8200a23bc173b5694b3fbb26
and3ss <anderson.aluizio@outlook.com>
2024-12-27 09:27:27 -0300

    Add Portal and Post models with migrations and seed data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Portal;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Portal::insert(
+            [
+                ['name' => 'Maranhão', 'url' => 'https://g1.globo.com/ma/maranhao/'],
+                ['name' => 'Piauí', 'url' => 'https://g1.globo.com/pi/piaui/'],
+                ['name' => 'Ceará', 'url' => 'https://g1.globo.com/ce/ceara/'],
+                ['name' => 'Rio Grande do Norte', 'url' => 'https://g1.globo.com/rn/rio-grande-do-norte/'],
+                ['name' => 'Paraíba', 'url' => 'https://g1.globo.com/pb/paraiba/'],
+                ['name' => 'Pernambuco', 'url' => 'https://g1.globo.com/pe/pernambuco/'],
+                ['name' => 'Alagoas', 'url' => 'https://g1.globo.com/al/alagoas/'],
+                ['name' => 'Sergipe', 'url' => 'https://g1.globo.com/se/sergipe/'],
+                ['name' => 'Bahia', 'url' => 'https://g1.globo.com/ba/bahia/'],
+            ]
+        );
     }
 }
